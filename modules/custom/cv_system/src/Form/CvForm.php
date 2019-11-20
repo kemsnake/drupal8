328595d7d2e7f7c4ab6e6ac402b5f9286d47a0cd
Faizulin Ivan <kemsnake@yandex.ru>
2019-11-20 11:34:16 +0300

    Test task #xxx. Created custom db table and save form data to it.

--- modules/custom/cv_system/src/Form/CvForm.php
+++ modules/custom/cv_system/src/Form/CvForm.php
@@ -3,20 +3,13 @@
  * @file
  * Contains Drupal\cv_system\Form\MessagesForm.
  */
+
 namespace Drupal\cv_system\Form;
-use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Database\Database;
+use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
-class CvForm extends ConfigFormBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function getEditableConfigNames() {
-    return [
-      'cv_system.adminsettings',
-    ];
-  }
+class CvForm extends FormBase {
 
   /**
    * {@inheritdoc}
@@ -33,43 +26,79 @@ class CvForm extends ConfigFormBase {
 
     $form['name'] = [
       '#type' => 'textfield',
+      '#required' => TRUE,
       '#title' => $this->t('Name'),
       '#description' => $this->t('User first name'),
       '#default_value' => $config->get('name'),
     ];
     $form['surname'] = [
       '#type' => 'textfield',
+      '#required' => TRUE,
       '#title' => $this->t('Surname'),
       '#description' => $this->t('User last name'),
       '#default_value' => $config->get('surname'),
     ];
     $form['email'] = [
       '#type' => 'textfield',
+      '#required' => TRUE,
       '#title' => $this->t('Email'),
       '#description' => $this->t('User email'),
       '#default_value' => $config->get('email'),
     ];
-    $form['cv_file'] = [
-      '#type' => 'file',
-      '#title' => $this->t('Upload CV'),
-      '#description' => $this->t('CV file'),
-      '#default_value' => $config->get('cv_file'),
+    $form['cv_file'] = array(
+      '#type' => 'managed_file',
+      '#name' => 'cv_file',
+      '#required' => TRUE,
+      '#title' => t('Upload CV'),
+      '#size' => 20,
+      '#description' => t('PDF and MS word format only'),
+      '#upload_validators' => ['file_validate_extensions' => ['pdf', 'docx', 'doc']],
+      '#upload_location' => 'public://cv_files/',
+    );
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Send CV'),
     ];
 
-    return parent::buildForm($form, $form_state);
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // Validate name length.
+    if (strlen($form_state->getValue('name')) < 3) {
+      $form_state->setErrorByName('name', $this->t('The name should contain more then 2 symbol.'));
+    }
+    // Validate name length.
+    if (strlen($form_state->getValue('surname')) < 3) {
+      $form_state->setErrorByName('surname', $this->t('The surname should contain more then 2 symbol.'));
+    }
+    // Validate email.
+    if (!\Drupal::service('email.validator')->isValid($form_state->getValue('email'))) {
+      $form_state->setErrorByName('email', $this->t('Please provide a valid email'));
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    parent::submitForm($form, $form_state);
 
-    $this->config('cv_system.adminsettings')
-      ->set('name', $form_state->getValue('name'))
-      ->set('surname', $form_state->getValue('surname'))
-      ->set('email', $form_state->getValue('email'))
-      ->set('cv_file', $form_state->getValue('cv_file'))
-      ->save();
+    $form_file = $form_state->getValue('cv_file', 0);
+    // Insert data to custom table.
+    Database::getConnection()->insert('cv_system')
+      ->fields([
+        'name' => $form_state->getValue('name'),
+        'surname' => $form_state->getValue('surname'),
+        'email' => $form_state->getValue('email'),
+        'created' => \Drupal::time()->getRequestTime(),
+        'cv_fid' => $form_file[0],
+      ])
+      ->execute();
+
+    // Redirect user to thank you page.
+    $form_state->setRedirect('cv_system.cv_thanks');
   }
 }
